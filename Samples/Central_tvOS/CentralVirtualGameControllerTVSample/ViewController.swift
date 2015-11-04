8df61e6e9e7665f358ad7cf764bfbb7c85f7bdea
Rob Reuss <rob@simplyformed.net>
2015-11-04 15:46:35 -0800

    Re-add Central tvOS

---
+++ Samples/Central_tvOS/CentralVirtualGameControllerTVSample/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  CentralVirtualGameControllerTVSample
+//
+//  Created by Rob Reuss on 9/13/15.
+//  Copyright © 2015 Rob Reuss. All rights reserved.
+//
+
+import VirtualGameController
+
+class ViewController: VgcCentralViewController {
+    
+    override func viewDidLoad() {
+        
+        // Publishes the central service
+        VgcManager.startAs(.Central, customElements: CustomElements(), customMappings: CustomMappings())
+        
+        super.viewDidLoad()
+        
+    }
+
+}
+
