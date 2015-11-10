0719d45103a6a6504994e5260500ad12dc6b4ba0
Rob Reuss <rob@simplyformed.net>
2015-11-09 20:28:05 -0800

    Removed build folder

--- Build/Intermediates/VirtualGameControllerIOS.build/Debug-iphoneos/VirtualGameControllerIOS.build/Objects-normal/arm64/VirtualGameController_vers.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/robreuss/Documents/Development/VirtualGameController/Build/Intermediates/VirtualGameControllerIOS.build/Debug-iphoneos/VirtualGameControllerIOS.build/DerivedSources/VirtualGameController_vers.c
