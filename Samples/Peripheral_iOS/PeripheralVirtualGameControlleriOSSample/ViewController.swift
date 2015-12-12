fe5aeddf33cac6396d901645cf58a664efa30ca1
Rob Reuss <rob@simplyformed.net>
2015-12-11 20:43:41 -0800

    Adjustment to default motion update interval on iOS sample app

--- Samples/Peripheral_iOS/PeripheralVirtualGameControlleriOSSample/ViewController.swift
+++ Samples/Peripheral_iOS/PeripheralVirtualGameControlleriOSSample/ViewController.swift
@@ -50,7 +50,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         
         VgcManager.includesPeerToPeer = false
         
-        VgcManager.peripheral.motion.updateInterval = 1/60
+        VgcManager.peripheral.motion.updateInterval = 1/40
         
         VgcManager.peripheral.motion.enableAttitude = true
         VgcManager.peripheral.motion.enableGravity = true
