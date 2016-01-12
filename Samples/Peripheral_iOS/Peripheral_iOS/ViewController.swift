1909d4a731c3fd99bb1f4c01b70301cd4b130718
Rob Reuss <rob@simplyformed.net>
2016-01-11 19:27:48 -0800

    Changed Peripheral motion defaults

--- Samples/Peripheral_iOS/Peripheral_iOS/ViewController.swift
+++ Samples/Peripheral_iOS/Peripheral_iOS/ViewController.swift
@@ -66,9 +66,9 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         VgcManager.peripheral.motion.updateInterval = 1/60
         
         VgcManager.peripheral.motion.enableAttitude = true
-        VgcManager.peripheral.motion.enableGravity = true
-        VgcManager.peripheral.motion.enableRotationRate = true
-        VgcManager.peripheral.motion.enableUserAcceleration = true
+        VgcManager.peripheral.motion.enableGravity = false
+        VgcManager.peripheral.motion.enableRotationRate = false
+        VgcManager.peripheral.motion.enableUserAcceleration = false
         
         VgcManager.peripheral.motion.enableAdaptiveFilter = true
         VgcManager.peripheral.motion.enableLowPassFilter = true
