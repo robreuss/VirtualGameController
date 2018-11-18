98b116f5bbd3d107e61325f959fbaa09b4a29d30
Rob Reuss <robreuss@mac.com>
2018-11-18 10:48:57 -0500

    tvOS scheme update for tests

---
+++ Framework Projects/VirtualGameControllerTvOS/VirtualGameControllerTvOS_Tests/VirtualGameControllerTvOS_Tests.swift
@@ -0,0 +1,33 @@
+//
+//  VirtualGameControllerTvOS_Tests.swift
+//  VirtualGameControllerTvOS_Tests
+//
+//  Created by Rob Reuss on 11/18/18.
+//  Copyright © 2018 Rob Reuss. All rights reserved.
+//
+
+import XCTest
+
+class VirtualGameControllerTvOS_Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
